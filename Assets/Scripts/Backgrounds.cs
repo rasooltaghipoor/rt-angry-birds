96afa1cae53bb6809b62afbeba8d93238e3b6387
Rasoul Taghipour <78169463+rasooltaghipoor@users.noreply.github.com>
2023-05-15 09:34:23 +0330

    Extend game environmanet (#6)

---
+++ Assets/Scripts/Backgrounds.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgrounds : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _camera;
+    [SerializeField]
+    private float _deltaX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
